385582c3338ea882e46af64b58c6c6eb1b2cf569
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-12-07 20:35:53 +0900

    update: Update home

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, StickerSmash!</Text>
       <StatusBar style="auto" />
     </View>
   );
