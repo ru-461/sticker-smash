cf27d295050063310d02ccc15f49b00f9ae56f63
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-12-13 20:10:50 +0900

    chore: Bool value replacement for codeActionsOnSave

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-import { StatusBar } from 'expo-status-bar';
-
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello, StickerSmash!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
