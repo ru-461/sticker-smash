ba2ffd0052850d18bdfbce2044ce7d1252f2e726
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-12-16 11:18:21 +0900

    update: Build a screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('../assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, StickerSmash!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer PlaceholderImage={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -11,8 +25,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
